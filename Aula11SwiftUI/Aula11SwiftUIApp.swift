7679c4c9850ee16faab1375d743ae8a9e8a6e9b2
johnnydev-tech <johnnyfreirejh@hotmail.com>
2025-10-29 20:04:29 -0300

    Initial Commit

---
+++ Aula11SwiftUI/Aula11SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula11SwiftUIApp.swift
+//  Aula11SwiftUI
+//
+//  Created by Johnny Freire on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Aula11SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
